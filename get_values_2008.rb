a23edda157c485ca5a736154d7b9e1238f748294
Stein Wang <SteinW@Steins-MacBook-Pro-2.local>
2013-06-12 16:25:43 -0400

    value of 2008 added

---
+++ get_values_2008.rb
@@ -0,0 +1,15 @@
+require 'rubygems'
+require 'nokogiri'
+require 'open-uri'
+
+File.open('values_2008.txt', 'w') do |f|
+
+	page = Nokogiri::HTML(open("http://data.worldbank.org/indicator/EN.ATM.CO2E.KT/countries/1W?display=default"))
+
+	tteight_values = page.css('.views-field-wbapi-data-value-2008')
+
+	tteight_values.each do |tteight|
+		tteight = tteight.text.strip
+		f.write(tteight + "\n") if !tteight.nil? && tteight != "2008"
+	end
+end
