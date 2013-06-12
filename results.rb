05121ac279cc1cee1e09aaaaeaf0bad8a8645537
Stein Wang <SteinW@Steins-MacBook-Pro-2.local>
2013-06-12 18:56:28 -0400

    search result working, txt file added

--- results.rb
+++ results.rb
@@ -1,26 +1,20 @@
-countries = File.read("countries.txt")
-puts countries.class
+data = File.read("countries.txt")
+countries = data.split(/$\s/)
 
-# data_v2 = File.read("values_2009.txt")
-# values_2009 = data_v2.split
+data_v2 = File.read("values_2009.txt")
+values_2009 = data_v2.split
 
-# results_hash = {}
-# i = 0
+results_hash = {}
+i = 0
 
-# countries.each do |country|
-# 	results_hash[country] = values_2009[i]
-# 	i += 1
-# end
+countries.each do |country|
+	results_hash[country] = values_2009[i]
+	i += 1
+end
 
-# File.open('search_results.txt', 'w') do |f|
-# 	results_hash.each do |k, v|
-# 		f.write(k + "\n")
-# 		f.write(v.to_s + "\n")
-# 	end
-# end
-
-# File.open('paired_result.txt', 'w') do |f|
-# 	countries.each do |country|
-
-
-# end
+File.open('search_results.txt', 'w') do |f|
+	results_hash.each do |k, v|
+		f.write(k + "\n")
+		f.write(v.to_s + "\n")
+	end
+end
